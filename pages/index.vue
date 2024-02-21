bf9b84a9aa1248347da54d364429ccea5e5f32c1
Konstantin <tuskancik226@gmail.com>
2024-02-21 14:08:47 +0300

    add calendar component

--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,41 @@
 <template>
   <main class="main">
+    <VDatePicker
+      v-model="date"
+      :attributes="attrs"
+    />
 
+    <DateEventAdd :date="date" />
+
+    <DateEventsList :date="date" />
   </main>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import DateEventsList from '@/components/DateEventsList.vue'
+import DateEventAdd from '@/components/DateEventAdd.vue'
+import { ref } from 'vue';
+
+const date = ref(new Date())
+
+const attrs = ref([
+  {
+    key: 'today',
+    highlight: {
+      color: 'green',
+      fillMode: 'solid'
+    },
+    dates: new Date()
+  }
+])
+</script>
 
 <style scoped>
 .main {
   padding-top: 1rem;
   display: flex;
-  flex-direction: column;
-  align-items: center;
+  align-items: stretch;
   gap: 2rem;
+  width: 100%;
 }
 </style>
